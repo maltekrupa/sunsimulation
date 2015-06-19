9396a6aeb881db45617bce992e8266cedea1d989
Malte Krupa <mkrupa@nafn.de>
2015-06-19 21:23:43 +0200

    A Box

---
+++ js/custom.js
@@ -0,0 +1,63 @@
+window.onload = function() {
+    var renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    var scene = new THREE.Scene();
+
+    var camera = new THREE.PerspectiveCamera(
+        35,                                             // Field of view
+        window.innerWidth / window.innerHeight,         // Aspect ratio
+        0.1,                                            // Near plane
+        10000                                           // Far plane
+    );
+    camera.position.set( -15, 10, 10 );
+    camera.lookAt( scene.position );
+
+    var geometry = new THREE.CubeGeometry( 5, 5, 5 );
+    var material = new THREE.MeshLambertMaterial( { color: 0xFF0000 } );
+    var mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+
+    var light = new THREE.PointLight( 0xFFFF00 );
+    light.position.set( 10, 0, 10 );
+    scene.add( light );
+
+    renderer.setClearColor( 0xdddddd, 1);
+    renderer.render( scene, camera );
+
+};
+
+function buildAxes( length ) {
+        var axes = new THREE.Object3D();
+
+        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( length, 0, 0 ), 0xFF0000, false ) ); // +X
+        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( -length, 0, 0 ), 0xFF0000, true) ); // -X
+        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, length, 0 ), 0x00FF00, false ) ); // +Y
+        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, -length, 0 ), 0x00FF00, true ) ); // -Y
+        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, length ), 0x0000FF, false ) ); // +Z
+        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, -length ), 0x0000FF, true ) ); // -Z
+
+        return axes;
+
+};
+
+function buildAxis( src, dst, colorHex, dashed ) {
+        var geom = new THREE.Geometry(),
+            mat; 
+
+        if(dashed) {
+                mat = new THREE.LineDashedMaterial({ linewidth: 3, color: colorHex, dashSize: 3, gapSize: 3 });
+        } else {
+                mat = new THREE.LineBasicMaterial({ linewidth: 3, color: colorHex });
+        }
+
+        geom.vertices.push( src.clone() );
+        geom.vertices.push( dst.clone() );
+        geom.computeLineDistances(); // This one is SUPER important, otherwise dashed lines will appear as simple plain lines
+
+        var axis = new THREE.Line( geom, mat, THREE.LinePieces );
+
+        return axis;
+
+};
